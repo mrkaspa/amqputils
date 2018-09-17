16e559d0501fc5cfccb2c2a41f1824667852e3ed
Juliana Davila R <judaro13@gmail.com>
2018-09-17 10:41:04 -0500

    added simple healtcheck

--- server.go
+++ server.go
@@ -32,6 +32,23 @@ func NewServer(version string, ch *amqp.Channel, event string, do SubscribeFunc,
 	}, nil
 }
 
+// HealtCheck send a response each time that receive a message
+func HealtCheck(ch *amqp.Channel, queueService string) (*Server, error) {
+	q, err := CreateQueue(ch, queueService)
+	if err != nil {
+		return nil, err
+	}
+	return &Server{
+		Event: queueService,
+		Do: func(m amqp.Delivery) []byte {
+			return []byte(queueService + " active ")
+		},
+		AMQPChan:       ch,
+		AMQPQueue:      q,
+		CompareVersion: func(a, b string) (bool, []byte) { return true, nil },
+	}, nil
+}
+
 // Start the server
 func (s *Server) Start() {
 	f := func(delivery amqp.Delivery) []byte {
